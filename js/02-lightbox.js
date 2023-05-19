7c4c43a916d9209c0d84a59823198755c4a116cd
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-05-19 17:34:15 +0200

    add js task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            </li>
+        `;
+    }).join("");
+}
+
+galleryEl.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+})
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+})
 console.log(galleryItems);
